a8fa2e4447b4b5b675945d8fc9d53b25fad09ae0
majid <mjamali@ars-network.com>
2023-03-20 11:44:09 +0330

    Adding base project

---
+++ CopyPasteBoardApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CopyPasteBoardApp
+//
+//  Created by Majid on 20/03/2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
